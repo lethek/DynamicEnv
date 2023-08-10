54f88a155ba645ebe694a9b57f79eb1afd4bd955
Michael Monsour <michael@lethek.net>
2023-08-10 15:29:43 +1000

    Added XML documentation comments and modernized the code a bit

--- src/DynamicEnv/Env.cs
+++ src/DynamicEnv/Env.cs
@@ -5,101 +5,164 @@ using System.Dynamic;
 using System.Linq;
 
 
-namespace DynamicEnv
+namespace DynamicEnv;
+
+/// <summary>
+/// Represents a dynamic environment variable object which can be accessed or updated in a code-friendly manner.
+/// </summary>
+public class Env : DynamicObject
 {
 
-    public class Env : DynamicObject
+    /// <summary>
+    /// Gets or sets the <see cref="Env"/> instance associated with the current process.
+    /// </summary>
+    public static readonly dynamic Process = new Env(EnvironmentVariableTarget.Process);
+
+    /// <summary>
+    /// Gets or sets the <see cref="Env"/> instance associated with the current user.
+    /// </summary>
+    public static readonly dynamic User = new Env(EnvironmentVariableTarget.User);
+
+    /// <summary>
+    /// Gets or sets the <see cref="Env"/> instance associated with the local machine.
+    /// </summary>
+    public static readonly dynamic Machine = new Env(EnvironmentVariableTarget.Machine);
+
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Env"/> class with the environment block associated with the current process.
+    /// </summary>
+    public Env() : this(EnvironmentVariableTarget.Process) { }
+
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Env"/> class with specified target.
+    /// </summary>
+    /// <param name="target">The target environment variable scope.</param>
+    public Env(EnvironmentVariableTarget target)
+        => _target = target;
+
+
+    /// <summary>
+    /// Tries to get the value of an environment variable by a key.
+    /// </summary>
+    /// <param name="binder">The dynamic get member binder.</param>
+    /// <param name="result">The result value.</param>
+    /// <returns>Always returns <see langword="true"/>. If an environment variable associated with key is not found, the <paramref name="result"/> will be <see langword="null"/>.</returns>
+    public override bool TryGetMember(GetMemberBinder binder, out object? result)
     {
-        public static readonly dynamic Process = new Env(EnvironmentVariableTarget.Process);
-        public static readonly dynamic User = new Env(EnvironmentVariableTarget.User);
-        public static readonly dynamic Machine = new Env(EnvironmentVariableTarget.Machine);
-
-
-        public Env() : this(EnvironmentVariableTarget.Process) { }
-
-
-        public Env(EnvironmentVariableTarget target)
-            => _target = target;
-
-
-        public override bool TryGetMember(GetMemberBinder binder, out object result)
-        {
-            result = GetEnvVar(binder.Name);
-            return true;
-        }
-
-
-        public override bool TrySetMember(SetMemberBinder binder, object value)
-        {
-            SetEnvVar(binder.Name, value);
-            return true;
-        }
-
-
-        [ExcludeFromCodeCoverage]
-        public override bool TryDeleteMember(DeleteMemberBinder binder)
-        {
-            DelEnvVar(binder.Name);
-            return true;
-        }
-
-
-        public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result)
-        {
-            result = GetEnvVar(GetEnvKeyFromIndexes(indexes));
-            return true;
-        }
-
-
-        public override bool TrySetIndex(SetIndexBinder binder, object[] indexes, object value)
-        {
-            SetEnvVar(GetEnvKeyFromIndexes(indexes), value);
-            return true;
-        }
-
-
-        [ExcludeFromCodeCoverage]
-        public override bool TryDeleteIndex(DeleteIndexBinder binder, object[] indexes)
-        {
-            DelEnvVar(GetEnvKeyFromIndexes(indexes));
-            return true;
-        }
-
-
-        public override IEnumerable<string> GetDynamicMemberNames()
-            => Environment.GetEnvironmentVariables(_target).Keys.Cast<string>();
-
-
-        private string GetEnvVar(string name)
-            => Environment.GetEnvironmentVariable(name, _target);
-
-
-        private void SetEnvVar(string name, object value)
-        {
-            if (value != null && value.GetType() != typeof(string)) {
-                throw new ArgumentException("Value must be of type String", nameof(value));
-            }
-
-            Environment.SetEnvironmentVariable(name, (string)value, _target);
-        }
-
-
-        [ExcludeFromCodeCoverage]
-        private void DelEnvVar(string name)
-            => Environment.SetEnvironmentVariable(name, null);
-
-
-        private string GetEnvKeyFromIndexes(object[] indexes)
-        {
-            try {
-                return indexes.OfType<string>().Single();
-            } catch {
-                throw new IndexOutOfRangeException("Index must be of type String");
-            }
-        }
-
-
-        private readonly EnvironmentVariableTarget _target;
+        result = GetEnvVar(binder.Name);
+        return true;
     }
 
+
+    /// <summary>
+    /// Tries to set the value of an environment variable by a key.
+    /// </summary>
+    /// <param name="binder">The dynamic set member binder.</param>
+    /// <param name="value">The value to set.</param>
+    /// <returns>Always returns <see langword="true"/>.</returns>
+    public override bool TrySetMember(SetMemberBinder binder, object? value)
+    {
+        SetEnvVar(binder.Name, value);
+        return true;
+    }
+
+
+    /// <summary>
+    /// Tries to delete the value of an environment variable by a key. This is achieved by setting the value to <see langword="null"/>.
+    /// </summary>
+    /// <param name="binder">The dynamic delete member binder.</param>
+    /// <returns>Always returns <see langword="true"/>.</returns>
+    [ExcludeFromCodeCoverage]
+    public override bool TryDeleteMember(DeleteMemberBinder binder)
+    {
+        DelEnvVar(binder.Name);
+        return true;
+    }
+
+
+    /// <summary>
+    /// Tries to get a value of an environment variable by an index array of strings.
+    /// </summary>
+    /// <param name="binder">The dynamic get index binder.</param>
+    /// <param name="indexes">The index array of strings. </param>
+    /// <param name="result">The result value.</param>
+    /// <returns>Always returns <see langword="true"/>.</returns>
+    /// <exception cref="IndexOutOfRangeException">Thrown when no environment variable with the specified index is found.</exception>
+    public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object? result)
+    {
+        result = GetEnvVar(GetEnvKeyFromIndexes(indexes));
+        return true;
+    }
+
+
+    /// <summary>
+    /// Tries to set a value to an environment variable by an index array of strings.
+    /// </summary>
+    /// <param name="binder">The dynamic set index binder.</param>
+    /// <param name="indexes">The index array of strings. </param>
+    /// <param name="value">The value to set.</param>
+    /// <returns>Always returns <see langword="true"/>.</returns>
+    /// <exception cref="IndexOutOfRangeException">Thrown when no environment variable with the specified index is found.</exception>
+    public override bool TrySetIndex(SetIndexBinder binder, object[] indexes, object? value)
+    {
+        SetEnvVar(GetEnvKeyFromIndexes(indexes), value);
+        return true;
+    }
+
+
+    /// <summary>
+    /// Tries to delete a value of an environment variable by an index array of strings.
+    /// </summary>
+    /// <param name="binder">The dynamic delete index binder.</param>
+    /// <param name="indexes">The index array of strings. </param>
+    /// <returns>Always returns <see langword="true"/>.</returns>
+    /// <exception cref="IndexOutOfRangeException">Thrown when no environment variable with the specified index is found.</exception>
+    [ExcludeFromCodeCoverage]
+    public override bool TryDeleteIndex(DeleteIndexBinder binder, object[] indexes)
+    {
+        DelEnvVar(GetEnvKeyFromIndexes(indexes));
+        return true;
+    }
+
+
+    /// <summary>
+    /// Returns the list of the environment variable keys.
+    /// </summary>
+    /// <returns>A list of the environment variable keys.</returns>
+    public override IEnumerable<string> GetDynamicMemberNames()
+        => Environment.GetEnvironmentVariables(_target).Keys.Cast<string>();
+
+
+    private string? GetEnvVar(string name)
+        => Environment.GetEnvironmentVariable(name, _target);
+
+
+    private void SetEnvVar(string name, object? value)
+    {
+        if (value != null && value.GetType() != typeof(string)) {
+            throw new ArgumentException("Value must be of type String", nameof(value));
+        }
+
+        Environment.SetEnvironmentVariable(name, (string?)value, _target);
+    }
+
+
+    [ExcludeFromCodeCoverage]
+    private void DelEnvVar(string name)
+        => Environment.SetEnvironmentVariable(name, null);
+
+
+    private string GetEnvKeyFromIndexes(object[] indexes)
+    {
+        try {
+            return indexes.OfType<string>().Single();
+        } catch {
+            throw new IndexOutOfRangeException("Index must be of type String");
+        }
+    }
+
+
+    private readonly EnvironmentVariableTarget _target;
 }
--- tests/DynamicEnv.Tests/EnvTests.cs
+++ tests/DynamicEnv.Tests/EnvTests.cs
@@ -1,132 +1,125 @@
-using System;
-using System.Collections.Generic;
+namespace DynamicEnv;
 
-using Xunit;
-
-
-namespace DynamicEnv
+public class EnvTests
 {
-
-    public class EnvTests
+    [Fact]
+    public void Env_StaticInstances_AreNotNull()
     {
-        [Fact]
-        public void Env_StaticInstances_AreNotNull()
-        {
-            Assert.NotNull(Env.Process);
-            Assert.NotNull(Env.User);
-            Assert.NotNull(Env.Machine);
+        Assert.NotNull(Env.Process);
+        Assert.NotNull(Env.User);
+        Assert.NotNull(Env.Machine);
+    }
+
+
+    [Fact]
+    public void Env_GetMissingVariable_ReturnsNull()
+    {
+        dynamic env = new Env();
+        Assert.Null(env.TEST_MISSING_VAR);
+        Assert.Null(Env.Process.TEST_MISSING_VAR);
+        Assert.Null(Env.User.TEST_MISSING_VAR);
+        Assert.Null(Env.Machine.TEST_MISSING_VAR);
+    }
+
+
+    [Fact]
+    public void Env_GetMissingVariableByIndex_ReturnsNull()
+    {
+        dynamic env = new Env();
+        Assert.Null(env["TEST_MISSING_BYINDEX"]);
+        Assert.Null(Env.Process["TEST_MISSING_BYINDEX"]);
+        Assert.Null(Env.User["TEST_MISSING_BYINDEX"]);
+        Assert.Null(Env.Machine["TEST_MISSING_BYINDEX"]);
+    }
+
+
+    [Fact]
+    public void Env_SetVariableWithString_NoErrors()
+    {
+        dynamic env = new Env();
+        env.TEST_STRING = "testing";
+    }
+
+
+    [Fact]
+    public void Env_SetVariableWithStringByIndex_NoErrors()
+    {
+        dynamic env = new Env();
+        env["TEST_STRING_BYINDEX"] = "testing";
+    }
+
+
+    [Fact]
+    public void Env_SetAndGetVariable_ReturnsSameValue()
+    {
+        dynamic env = new Env();
+        var value = Guid.NewGuid().ToString();
+        env.TEST_SETGET = value;
+        Assert.StrictEqual(value, env.TEST_SETGET);
+    }
+
+
+    [Fact]
+    public void Env_SetAndGetVariableByIndex_ReturnsSameValue()
+    {
+        dynamic env = new Env();
+        var value = Guid.NewGuid().ToString();
+        env["TEST_SETGET_BYINDEX"] = value;
+        Assert.StrictEqual(value, env["TEST_SETGET_BYINDEX"]);
+    }
+
+
+    [Fact]
+    public void Env_SetInvalidType_ThrowsArgumentException()
+    {
+        dynamic env = new Env();
+        var obj = new Object();
+        Assert.Throws<ArgumentException>("value", () => env.TEST_INVALIDTYPE = obj);
+    }
+
+
+    [Fact]
+    public void Env_SetInvalidTypeByIndex_ThrowsArgumentException()
+    {
+        dynamic env = new Env();
+        var obj = new Object();
+        Assert.Throws<ArgumentException>("value", () => env["TEST_INVALIDTYPE_BYINDEX"] = obj);
+    }
+
+
+    [Fact]
+    public void Env_GetDynamicMemberNames_ReturnsEnumerable()
+    {
+        dynamic env = new Env();
+        Assert.IsAssignableFrom<IEnumerable<string>>(env.GetDynamicMemberNames());
+        Assert.IsAssignableFrom<IEnumerable<string>>(Env.Process.GetDynamicMemberNames());
+        Assert.IsAssignableFrom<IEnumerable<string>>(Env.User.GetDynamicMemberNames());
+        Assert.IsAssignableFrom<IEnumerable<string>>(Env.Machine.GetDynamicMemberNames());
+
+        IEnumerable<string> varNames = env.GetDynamicMemberNames();
+        foreach (var key in varNames) {
+            string value = env[key];
+            Assert.NotNull(value);
         }
+    }
 
 
-        [Fact]
-        public void Env_GetMissingVariable_ReturnsNull()
-        {
-            dynamic env = new Env();
-            Assert.Null(env.TEST_MISSING_VAR);
-            Assert.Null(Env.Process.TEST_MISSING_VAR);
-            Assert.Null(Env.User.TEST_MISSING_VAR);
-            Assert.Null(Env.Machine.TEST_MISSING_VAR);
-        }
-
-
-        [Fact]
-        public void Env_GetMissingVariableByIndex_ReturnsNull()
-        {
-            dynamic env = new Env();
-            Assert.Null(env["TEST_MISSING_BYINDEX"]);
-            Assert.Null(Env.Process["TEST_MISSING_BYINDEX"]);
-            Assert.Null(Env.User["TEST_MISSING_BYINDEX"]);
-            Assert.Null(Env.Machine["TEST_MISSING_BYINDEX"]);
-        }
-
-
-        [Fact]
-        public void Env_SetVariableWithString_NoErrors()
-        {
-            dynamic env = new Env();
-            env.TEST_STRING = "testing";
-        }
-
-
-        [Fact]
-        public void Env_SetVariableWithStringByIndex_NoErrors()
-        {
-            dynamic env = new Env();
-            env["TEST_STRING_BYINDEX"] = "testing";
-        }
-
-
-        [Fact]
-        public void Env_SetAndGetVariable_ReturnsSameValue()
-        {
-            dynamic env = new Env();
-            string value = Guid.NewGuid().ToString();
-            env.TEST_SETGET = value;
-            Assert.StrictEqual(value, env.TEST_SETGET);
-        }
-
-
-        [Fact]
-        public void Env_SetAndGetVariableByIndex_ReturnsSameValue()
-        {
-            dynamic env = new Env();
-            string value = Guid.NewGuid().ToString();
-            env["TEST_SETGET_BYINDEX"] = value;
-            Assert.StrictEqual(value, env["TEST_SETGET_BYINDEX"]);
-        }
-
-
-        [Fact]
-        public void Env_SetInvalidType_ThrowsArgumentException()
-        {
-            dynamic env = new Env();
-            object obj = new Object();
-            Assert.Throws<ArgumentException>("value", () => env.TEST_INVALIDTYPE = obj);
-        }
-
-
-        [Fact]
-        public void Env_SetInvalidTypeByIndex_ThrowsArgumentException()
-        {
-            dynamic env = new Env();
-            object obj = new Object();
-            Assert.Throws<ArgumentException>("value", () => env["TEST_INVALIDTYPE_BYINDEX"] = obj);
-        }
-
-
-        [Fact]
-        public void Env_GetDynamicMemberNames_ReturnsEnumerable()
-        {
-            dynamic env = new Env();
-            Assert.IsAssignableFrom<IEnumerable<string>>(env.GetDynamicMemberNames());
-            Assert.IsAssignableFrom<IEnumerable<string>>(Env.Process.GetDynamicMemberNames());
-            Assert.IsAssignableFrom<IEnumerable<string>>(Env.User.GetDynamicMemberNames());
-            Assert.IsAssignableFrom<IEnumerable<string>>(Env.Machine.GetDynamicMemberNames());
-
-            IEnumerable<string> varNames = env.GetDynamicMemberNames();
-            foreach (string key in varNames) {
-                string value = env[key];
-            }
-        }
-
-
-        [Fact]
-        public void Env_GetByInvalidIndex_ThrowsIndexOutOfRangeException()
-        {
-            dynamic env = new Env();
+    [Fact]
+    public void Env_GetByInvalidIndex_ThrowsIndexOutOfRangeException()
+    {
+        dynamic env = new Env();
             
-            Assert.Throws<IndexOutOfRangeException>(() => env[0]);
-            Assert.Throws<IndexOutOfRangeException>(() => Env.Process[0]);
-            Assert.Throws<IndexOutOfRangeException>(() => Env.User[0]);
-            Assert.Throws<IndexOutOfRangeException>(() => Env.Machine[0]);
+        Assert.Throws<IndexOutOfRangeException>(() => env[0]);
+        Assert.Throws<IndexOutOfRangeException>(() => Env.Process[0]);
+        Assert.Throws<IndexOutOfRangeException>(() => Env.User[0]);
+        Assert.Throws<IndexOutOfRangeException>(() => Env.Machine[0]);
 
-            Assert.Throws<IndexOutOfRangeException>(() => env["TEST", "1"]);
-            Assert.Throws<IndexOutOfRangeException>(() => Env.Process["TEST", "1"]);
-            Assert.Throws<IndexOutOfRangeException>(() => Env.User["TEST", "1"]);
-            Assert.Throws<IndexOutOfRangeException>(() => Env.Machine["TEST", "1"]);
+        Assert.Throws<IndexOutOfRangeException>(() => env["TEST", "1"]);
+        Assert.Throws<IndexOutOfRangeException>(() => Env.Process["TEST", "1"]);
+        Assert.Throws<IndexOutOfRangeException>(() => Env.User["TEST", "1"]);
+        Assert.Throws<IndexOutOfRangeException>(() => Env.Machine["TEST", "1"]);
 
-            Console.WriteLine((string)env.ThisVarDoesntExist);
-            Console.WriteLine($"{env.ThisVarDoesntExist}");
-        }
+        Console.WriteLine((string)env.ThisVarDoesntExist);
+        Console.WriteLine($"{env.ThisVarDoesntExist}");
     }
 }
